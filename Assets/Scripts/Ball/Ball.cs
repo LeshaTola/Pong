20e24f9cbf78076d32c2bd7c582a51674c3d5dd6
LeshaTola <tola.lesha@mail.ru>
2023-09-24 12:40:36 +0300

    Add Ball logic Setup scene

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Speed")]
+	[SerializeField] private float startSpeed;
+	[SerializeField] private float maxSpeed;
+	[SerializeField] private float increaseValue;
+	[Header("Angle")]
+	[SerializeField, Range(0, 90)] private int deflectionAngle;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Rigidbody2D RB;
+	private float currentSpeed;
+	private Vector2 currentDirection;
+
+	public Vector2 StartDirection { get; private set; }
+
+	private void Awake()
+	{
+		RB = GetComponent<Rigidbody2D>();
+
+		currentSpeed = startSpeed;
+
+		SetCurentDirection(0f, Vector2.right);
+	}
+
+	public void SetCurentDirection(float factor, Vector2 frontVector)
+	{
+		var correctFactor = Math.Clamp(factor, -1f, 1f);
+		currentDirection = Quaternion.Euler(0, 0, (deflectionAngle * correctFactor) * (frontVector.x > 0 ? 1 : -1)) * frontVector;
+		RB.velocity = Vector2.zero;
+		RB.AddForce(currentDirection * currentSpeed, ForceMode2D.Impulse);
+	}
+
+	public void IncreaseSpeed()
+	{
+		currentSpeed += currentSpeed >= maxSpeed ? 0 : increaseValue;
+	}
 }
+
